6f482585ebcb9ebf56fc1eb725705f2fa60538b7
zjd <778450868@qq.com>
2019-08-22 15:42:29 +0800

    修改配置

--- zipkin/src/main/java/tk/mybatis/BaseMapper.java
+++ zipkin/src/main/java/tk/mybatis/BaseMapper.java
@@ -1,12 +1,12 @@
 package tk.mybatis;
 
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-
-/**
- * 特别注意，该接口不能被扫描到，否则会出错
- * @param <T>
- */
-public interface BaseMapper<T> extends Mapper<T>, MySqlMapper<T> {
-}
+//import tk.mybatis.mapper.common.Mapper;
+//import tk.mybatis.mapper.common.MySqlMapper;
+//
+//
+///**
+// * 特别注意，该接口不能被扫描到，否则会出错
+// * @param <T>
+// */
+//public interface BaseMapper<T> extends Mapper<T>, MySqlMapper<T> {
+//}
